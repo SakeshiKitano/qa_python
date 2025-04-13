7e06a35707bcb7055906e833e57629b3356cafe2
Said Ismailov <cyberpunksay@gmail.com>
2025-04-13 17:33:23 +0500

    First commit

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 from main import BooksCollector
-
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -22,3 +21,4 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_
